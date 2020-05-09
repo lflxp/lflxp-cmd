a1d502ff118654426515d58666b57ec48b44e6e9
yckj0834 <lixueping@cloudwalk.cn>
2020-05-09 20:11:02 +0800

    add ssh

--- cmd/main.go
+++ cmd/main.go
@@ -5,3 +5,9 @@ import "github.com/lflxp/lflxp-cmd/pkg"
 func main() {
 	pkg.Exec()
 }
+
+// func main() {
+//     cli := ssh.Ssh("1.1.1.1", "root", "xxx", 22)
+//     output, err := cli.Run("df -h")
+//     fmt.Printf("%v\n%v", output, err)
+// }
--- go.mod
+++ go.mod
@@ -13,4 +13,5 @@ require (
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.3
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 )
